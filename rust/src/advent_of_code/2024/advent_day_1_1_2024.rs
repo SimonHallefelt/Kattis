823f88ee4e8f4e54cf593225489552609eed8e2b
Simon Hallefält <simon@hallefelt.se>
2024-12-01 12:48:22 +0100

    start advent

---
+++ rust/src/advent_of_code/2024/advent_day_1_1_2024.rs
@@ -0,0 +1,12 @@
+
+
+fn main() {
+    let file_path = "rust\\src\\advent_of_code\\2024\\data\\day_1.txt";
+    let contents = fs::read_to_string(file_path)
+        .expect("Should have been able to read the file");
+    let input = read_line(file_path);
+}
+
+fn read_line(file_path: String){
+    
+}
