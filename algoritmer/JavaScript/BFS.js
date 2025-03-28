ce64a0d2aeb6fa1ac5f96206798a9a9f62512cde
Simon Hallefält <simon@hallefelt.se>
2025-03-28 23:34:20 +0100

    added a test to JavaScript BFS

--- algoritmer/JavaScript/BFS.js
+++ algoritmer/JavaScript/BFS.js
@@ -27,3 +27,23 @@ function BFS(graph, startNode, endNode) {
 
     return path
 }
+
+function test1() {
+    let graph = {}
+    graph[1] = [2,3]
+    graph[2] = [3,4]
+    graph[3] = [2,3,4,5]
+    graph[4] = [2,3,4]
+    graph[5] = [4,6]
+    graph[6] = [5]
+    graph[7] = [8]
+    graph[8] = [7]
+    console.log(1 === BFS(graph, 1, 1).length);
+    console.log(4 === BFS(graph, 1, 6).length);
+    console.log(0 === BFS(graph, 1, 8).length);
+    console.log(2 === BFS(graph, 7, 8).length);
+    console.log(2 === BFS(graph, 8, 7).length);
+
+    console.log(0 !== BFS(graph, 7, 7).length);
+}
+test1()
