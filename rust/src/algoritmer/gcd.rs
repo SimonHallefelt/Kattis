b50ec59e701b48dd4de025f2544e3d4e77458a21
Simon Hallefält <simon@hallefelt.se>
2025-04-03 22:04:51 +0200

    added a test for rust gcd

--- rust/src/algoritmer/gcd.rs
+++ rust/src/algoritmer/gcd.rs
@@ -1,10 +1,24 @@
-pub fn gcd(mut n: i64, mut m: i64) -> i64 {
+fn gcd(mut n: i64, mut m: i64) -> i64 {
     assert!(n != 0 && m != 0);
     while m != 0 {
-      if m < n {
-        std::mem::swap(&mut m, &mut n);
-      }
-      m %= n;
+        if m < n {
+          std::mem::swap(&mut m, &mut n);
+        }
+        m %= n;
     }
     n
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_1() {
+        assert_eq!(1, gcd(11, 13));
+        assert_eq!(2, gcd(2, 8));
+        assert_eq!(3, gcd(6, 9));
+        assert_eq!(3, gcd(15, 18));
+        assert_eq!(6, gcd(12, 18));
+    }
 }
--- rust/src/algoritmer/mod.rs
+++ rust/src/algoritmer/mod.rs
@@ -1,2 +1,3 @@
 pub mod BFS;
 pub mod Gale_Shapley_stable_Matchings;
+pub mod gcd;
