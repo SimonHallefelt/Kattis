aed7d4f8896b4156a2a67afec7fe93f05737df72
Simon hallefält <simon@hallefelt.se>
2025-12-01 08:10:06 +0100

    code of advent day 1

---
+++ C#/AdventOfCode_Day1_1/Program.cs
@@ -0,0 +1,23 @@
+﻿var startValue = 50;
+
+var minValue = 0;
+var maxValue = 100;
+
+// var data = File.ReadAllLines("../../../ExampleData.txt"); // 3
+var data = File.ReadAllLines("../../../Data.txt"); // 1154
+
+var value = startValue;
+var result = 0;
+foreach (var line in data)
+{
+    var direction = line[0];
+    var distance = int.Parse(line.Substring(1));
+    value += direction == 'R' ? distance : -distance;
+    value %= maxValue;
+    value = (value + maxValue) % maxValue;
+    if (value == 0)
+        result++;
+    Console.WriteLine($"{value}, {result}");
+}
+
+Console.WriteLine(result);
---
+++ C#/AdventOfCode_Day1_2/Program.cs
@@ -0,0 +1,34 @@
+﻿var startValue = 50;
+
+var minValue = 0;
+var maxValue = 100;
+
+// var data = File.ReadAllLines("../../../ExampleData.txt"); // 6
+var data = File.ReadAllLines("../../../Data.txt"); // 6819
+
+var value = startValue;
+var result = 0;
+foreach (var line in data)
+{
+    var direction = line[0];
+    var distance = int.Parse(line.Substring(1));
+    var OldValue = value;
+    value += direction == 'R' ? distance : -distance;
+    if (OldValue == 0 && value < minValue)
+        value += maxValue;
+    while (value < minValue)
+    {
+        result++;
+        value += maxValue;
+    }
+    if (value == 0)
+        result++;
+    while (value >= maxValue)
+    {
+        result++;
+        value -= maxValue;
+    }
+    Console.WriteLine($"{value}, {line}, {OldValue}, {result}");
+}
+
+Console.WriteLine(result);
