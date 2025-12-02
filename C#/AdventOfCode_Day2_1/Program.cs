f72a1a32d5245f3fb86735ac68f95d50a770e7d5
Simon hallefält <simon@hallefelt.se>
2025-12-02 08:19:32 +0100

    code of advent day 2

---
+++ C#/AdventOfCode_Day2_1/Program.cs
@@ -0,0 +1,33 @@
+﻿// var data = File.ReadAllText("../../../ExampleData.txt"); // 1227775554
+var data = File.ReadAllText("../../../Data.txt"); // 12850231731
+
+var ranges = data.Split(",");
+long sum = 0;
+foreach(var range in ranges)
+{
+    var startValue = long.Parse(range.Split("-")[0]);
+    var endValue = long.Parse(range.Split("-")[1]);
+    for (long i = startValue; i <= endValue; i++)
+    {
+        var length = i.ToString().Count();
+        if (length != length / 2 * 2)
+        {
+            i = long.Parse(new String('9', i.ToString().Count()));
+            continue;
+        }
+
+        var startNumber = i.ToString().Substring(0, length / 2);
+        var endNumber = i.ToString().Substring(length / 2);
+
+        if (startNumber.Count() != endNumber.Count())
+        {
+            Console.WriteLine($"f {i}, {startNumber}, {endNumber}");
+            throw new Exception();
+        }
+
+        if (startNumber.Equals(endNumber))
+            sum += i;
+    }
+
+    Console.WriteLine($"{sum}");
+}
---
+++ C#/AdventOfCode_Day2_2/Program.cs
@@ -0,0 +1,33 @@
+﻿// var data = File.ReadAllText("../../../ExampleData.txt"); // 4174379265
+var data = File.ReadAllText("../../../Data.txt"); // 24774350322
+
+var ranges = data.Split(",");
+long sum = 0;
+foreach(var range in ranges)
+{
+    var startValue = long.Parse(range.Split("-")[0]);
+    var endValue = long.Parse(range.Split("-")[1]);
+    for (long i = startValue; i <= endValue; i++)
+    {
+        var length = i.ToString().Count();
+
+        for (int j = 1; j <= length / 2; j++)
+        {
+            if (length != length / j * j)
+                continue;
+            var hash = new HashSet<string>();
+            for (int k = 0; k < length; k += j)
+            {
+                hash.Add(i.ToString().Substring(k, j));
+            }
+            if (hash.Count() == 1)
+            {
+                sum += i;
+                Console.WriteLine($"{sum}, {i}");
+                break;
+            }
+        }
+    }
+
+    Console.WriteLine($"{sum}");
+}
