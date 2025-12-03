1dcb6f791313a6729fb3be604b7798555e7dff22
Simon hallefält <simon@hallefelt.se>
2025-12-03 08:42:39 +0100

    code of advent day 3

---
+++ C#/AdventOfCode_Day3_1/Program.cs
@@ -0,0 +1,24 @@
+﻿// var data = File.ReadAllLines("../../../ExampleData.txt"); // 357
+var data = File.ReadAllLines("../../../Data.txt"); // 17193
+
+var sum = 0;
+foreach(var line in data)
+{
+    var largest = 0;
+    var secondLargest = 0;
+    var lineLength = line.Count();
+    for (var i = 0; i < lineLength; i++)
+    {
+        var value = int.Parse(line[i].ToString());
+        if (value > largest && i+1 < lineLength)
+        {
+            largest = value;
+            secondLargest = 0;
+        }
+        else if (value > secondLargest)
+            secondLargest = value;
+    }
+    sum += largest * 10 + secondLargest;
+    Console.WriteLine($"{sum}, {largest * 10 + secondLargest}");
+}
+
---
+++ C#/AdventOfCode_Day3_2/Program.cs
@@ -0,0 +1,27 @@
+﻿// var data = File.ReadAllLines("../../../ExampleData.txt"); // 3121910778619
+var data = File.ReadAllLines("../../../Data.txt"); // 171297349921310
+
+long sum = 0;
+foreach(var line in data)
+{
+    var largest = Enumerable.Repeat(0, 12).ToList();
+    var lineLength = line.Count();
+    for (var i = 0; i < lineLength; i++)
+    {
+        var value = int.Parse(line[i].ToString());
+        var increased = false;
+        for (var j = 0; j < 12; j++)
+        {
+            if (increased)
+                largest[j] = 0;
+            else if (i - j + 11 < lineLength && value > largest[j])
+            {
+                largest[j] = value;
+                increased = true;
+            }
+        }
+    }
+    sum += long.Parse(string.Join("", largest));
+    Console.WriteLine($"{sum}, {long.Parse(string.Join("", largest))}");
+}
+
