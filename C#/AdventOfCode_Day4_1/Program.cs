9827de5a856b9756871557520820ad54ae834237
Simon hallefält <simon@hallefelt.se>
2025-12-04 19:39:31 +0100

    Advent of code day 4

---
+++ C#/AdventOfCode_Day4_1/Program.cs
@@ -0,0 +1,48 @@
+﻿// var data = File.ReadAllLines("../../../ExampleData.txt"); // 13
+var data = File.ReadAllLines("../../../Data.txt"); // 1433
+
+
+var map = new List<List<char>>();
+foreach (var line in data)
+{
+    map.Add(line.ToList());
+}
+
+var colLength = map.Count();
+var rowLength = map[0].Count();
+
+var sum = 0;
+
+for(var i = 0; i < rowLength; i++)
+{
+    for(var j = 0; j < colLength; j++)
+    {
+        if (map[i][j] != '@')
+            continue;
+        if (LookAtNeighbors(i, j) < 4)
+        {
+            sum++;
+            Console.WriteLine($"{sum}, {i}, {j}");
+        }
+    }
+}
+
+
+
+
+
+int LookAtNeighbors(int r, int c)
+{
+    var rolls = 0;
+    for(var i = Math.Max(0, r-1); i < Math.Min(r+2, rowLength); i++)
+    {
+        for(var j = Math.Max(0, c-1); j < Math.Min(c+2, colLength); j++)
+        {
+            if (i == r && j == c)
+                continue;
+            if (map[i][j] == '@')
+                rolls++;
+        }
+    }
+    return rolls;
+}
---
+++ C#/AdventOfCode_Day4_2/Program.cs
@@ -0,0 +1,47 @@
+﻿// var data = File.ReadAllLines("../../../ExampleData.txt"); // 43
+var data = File.ReadAllLines("../../../Data.txt"); // 8616
+
+
+var map = new List<List<char>>();
+foreach (var line in data)
+{
+    map.Add(line.ToList());
+}
+
+var colLength = map.Count();
+var rowLength = map[0].Count();
+
+var sum = 0;
+
+for(var i = 0; i < rowLength; i++)
+{
+    for(var j = 0; j < colLength; j++)
+    {
+        if (map[i][j] != '@')
+            continue;
+        if (LookAtNeighbors(i, j) < 4)
+        {
+            sum++;
+            map[i][j] = '.';
+            i = Math.Max(0, i - 1);
+            j = Math.Max(-1, j-2);
+        }
+    }
+}
+Console.WriteLine($"{sum}");
+
+int LookAtNeighbors(int r, int c)
+{
+    var rolls = 0;
+    for(var i = Math.Max(0, r-1); i < Math.Min(r+2, rowLength); i++)
+    {
+        for(var j = Math.Max(0, c-1); j < Math.Min(c+2, colLength); j++)
+        {
+            if (i == r && j == c)
+                continue;
+            if (map[i][j] == '@')
+                rolls++;
+        }
+    }
+    return rolls;
+}
