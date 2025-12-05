6f0a10e8e4af313155cc5f3ead0a3469ee01bb49
Simon hallefält <simon@hallefelt.se>
2025-12-05 21:22:50 +0100

    advent of code day 5

---
+++ C#/AdventOfCode_Day5_1/Program.cs
@@ -0,0 +1,32 @@
+﻿// var data = File.ReadAllLines("../../../ExampleData.txt"); // 3
+var data = File.ReadAllLines("../../../Data.txt"); // 652
+
+var ranges = new List<(long, long)>();
+var ids = new List<long>();
+
+foreach (var line in data)
+{
+    var l = line.Split("-");
+    if (l.Count() == 2)
+        ranges.Add((long.Parse(l[0]), long.Parse(l[1])));
+    else if (!string.IsNullOrEmpty(line))
+        ids.Add(long.Parse(l[0]));
+}
+
+ranges.Sort();
+ids.Sort();
+
+var sum = 0;
+var i = 0;
+foreach (var range in ranges)
+{
+    for (; i < ids.Count(); i++)
+    {
+        if (range.Item1 <= ids[i] && ids[i] <= range.Item2)
+            sum++;
+        else if (ids[i] > range.Item2)
+            break;
+    }
+}
+
+Console.WriteLine($"{sum}");
---
+++ C#/AdventOfCode_Day5_2/Program.cs
@@ -0,0 +1,31 @@
+﻿// var data = File.ReadAllLines("../../../ExampleData.txt"); // 14
+var data = File.ReadAllLines("../../../Data.txt"); // 341753674214273
+
+var ranges = new List<(long, long)>();
+var ids = new List<long>();
+
+foreach (var line in data)
+{
+    var l = line.Split("-");
+    if (l.Count() == 2)
+        ranges.Add((long.Parse(l[0]), long.Parse(l[1])));
+    else if (!string.IsNullOrEmpty(line))
+        break;
+}
+
+ranges.Sort();
+
+long sum = 0;
+for (var i = 0; i+1 < ranges.Count(); i++)
+{
+    if (ranges[i+1].Item1 <= ranges[i].Item2)
+        ranges[i+1] = (ranges[i].Item1, Math.Max(ranges[i].Item2, ranges[i+1].Item2));
+    else
+        sum += 1 + ranges[i].Item2 - ranges[i].Item1;
+    if (i + 2 == ranges.Count())
+        sum += 1 + ranges[i+1].Item2 - ranges[i+1].Item1;
+    
+    Console.WriteLine($"{sum}, {ranges[i]}, {ranges[i+1]}");
+}
+
+Console.WriteLine($"{sum}");
