3227b80c03675178db79489ee29437e3f1a556dd
Simon Hallefält <skip>
2025-12-14 18:53:26 +0100

    advet of code day 11

---
+++ C#/AdventOfCode_Day11_1/Program.cs
@@ -0,0 +1,28 @@
+﻿// var data = File.ReadAllLines("../../../ExampleData.txt"); // 5
+var data = File.ReadAllLines("../../../Data.txt"); // 658
+
+var map = new Dictionary<string, List<string>>();
+foreach (var line in data)
+{
+    var split = line.Split(": ").ToList();
+    map[split.First()] = split.Last().Split(" ").ToList();
+}
+
+var start = "you";
+var end = "out";
+var ends = new Dictionary<string, long>();
+var result = dfs(start, end);
+long dfs(string pos, string target)
+{
+    if (ends.ContainsKey(pos))
+        return ends[pos];
+    var sum = 0L;
+    foreach(var path in map[pos])
+    {
+        sum += path.Equals(target) ? 1 : dfs(path, target);
+    }
+    ends[pos] = sum;
+    return sum;
+}
+
+Console.WriteLine($"{result}");
---
+++ C#/AdventOfCode_Day11_2/Program.cs
@@ -0,0 +1,36 @@
+﻿// var data = File.ReadAllLines("../../../ExampleData.txt"); // 2
+var data = File.ReadAllLines("../../../Data.txt"); // 371113003846800
+
+var map = new Dictionary<string, List<string>>();
+foreach (var line in data)
+{
+    var split = line.Split(": ").ToList();
+    map[split.First()] = split.Last().Split(" ").ToList();
+}
+
+var start = "svr";
+var end = "out";
+var ends = new Dictionary<(string, bool, bool), long>();
+var result = dfs(start, end, false, false);
+long dfs(string pos, string target, bool fft, bool dac)
+{
+    var fftLocal = pos == "fft";
+    var dacLocal = pos == "dac";
+    if (ends.ContainsKey((pos, fft || fftLocal, dac || dacLocal)))
+        return ends[(pos, fft || fftLocal, dac || dacLocal)];
+    var sum = 0L;
+    foreach(var path in map[pos])
+    {
+        if (path.Equals(target))
+        {
+            if (fft && dac)
+                sum++;
+            continue;
+        }
+        sum += dfs(path, target, fft || fftLocal, dac || dacLocal);
+    }
+    ends[(pos, fft || fftLocal, dac || dacLocal)] = sum;
+    return sum;
+}
+
+Console.WriteLine($"{result}");
